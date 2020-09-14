90bdfb9926523ee3624f187533ad9f73460250e6
kajamarkowska <kajaa71@wp.pl>
2020-09-14 11:47:48 +0200

    added BasketRepository

--- src/main/java/com/kaja/shop/repository/BasketRepository.java
+++ src/main/java/com/kaja/shop/repository/BasketRepository.java
@@ -3,5 +3,11 @@ package com.kaja.shop.repository;
 import com.kaja.shop.domain.elastic.Basket;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
+import java.util.Optional;
+
 public interface BasketRepository extends ElasticsearchRepository<Basket, String> {
+
+
+
+    Optional<Basket> findByUserId (Long userId);
 }
