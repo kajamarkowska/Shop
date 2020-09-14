86fdac73ccf72a2e7d5d787a95905511137bee45
kajamarkowska <kajaa71@wp.pl>
2020-09-14 11:47:03 +0200

    added JwtAuthorizationFilter

--- src/main/java/com/kaja/shop/security/JwtAuthorizationFilter.java
+++ src/main/java/com/kaja/shop/security/JwtAuthorizationFilter.java
@@ -1,6 +1,12 @@
 package com.kaja.shop.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -8,6 +14,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager) {
@@ -21,5 +31,19 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
             chain.doFilter(request, response);
             return;
         }
+        Claims claims = Jwts.parser()
+                .setSigningKey("root")
+                .parseClaimsJws(token.replace("Bearer ", ""))
+                .getBody();
+
+        String email = claims.getSubject();
+        if(email != null){
+            String authorities = claims.get("authorities", String.class);
+            List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+            if(authorities != null){
+               grantedAuthorities = Arrays.stream(authorities.split(",")).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+            }
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(email, null, grantedAuthorities));
+        }else response.setStatus(401);
     }
 }
