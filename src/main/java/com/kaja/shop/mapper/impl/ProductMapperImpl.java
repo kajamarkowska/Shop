c69dc5fed27135f0ba171ac98c6dc1d796a0dc9f
kajamarkowska <kajaa71@wp.pl>
2020-09-16 20:33:11 +0200

    added ProductMapperImpl

--- src/main/java/com/kaja/shop/mapper/impl/ProductMapperImpl.java
+++ src/main/java/com/kaja/shop/mapper/impl/ProductMapperImpl.java
@@ -2,8 +2,12 @@ package com.kaja.shop.mapper.impl;
 
 import com.kaja.shop.domain.dao.Product;
 import com.kaja.shop.domain.dto.ProductDto;
+import com.kaja.shop.domain.elastic.ProductElastic;
 import com.kaja.shop.mapper.ProductMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductMapperImpl implements ProductMapper {
@@ -30,6 +34,24 @@ public class ProductMapperImpl implements ProductMapper {
                 .quantity(product.getQuantity())
                 .version(product.getVersion())
                 .build();
+    }
+
+    @Override
+    public ProductDto productElasticToProduct(ProductElastic productElastic) {
+        return ProductDto.builder()
+                .id(productElastic.getId())
+                .name(productElastic.getName())
+                .quantity(productElastic.getQuantity())
+                .price(productElastic.getPrice())
+                .build();
+
+    }
+
+    @Override
+    public List<ProductDto> mapToListProductDto(List<ProductElastic> productElastics) {
+        return productElastics.stream()
+                .map(this::productElasticToProduct)
+                .collect(Collectors.toList());
     }
 
 }
