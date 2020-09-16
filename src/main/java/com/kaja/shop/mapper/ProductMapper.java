2e3bbffd854b1e3f74779dd0d8b22c9aef070405
kajamarkowska <kajaa71@wp.pl>
2020-09-16 20:32:36 +0200

    added ProductMapper

--- src/main/java/com/kaja/shop/mapper/ProductMapper.java
+++ src/main/java/com/kaja/shop/mapper/ProductMapper.java
@@ -2,10 +2,18 @@ package com.kaja.shop.mapper;
 
 import com.kaja.shop.domain.dao.Product;
 import com.kaja.shop.domain.dto.ProductDto;
+import com.kaja.shop.domain.elastic.ProductElastic;
+
+import java.util.List;
 
 public interface ProductMapper {
 
     Product productDtoToProduct(ProductDto productDto);
 
     ProductDto productToProductDto(Product product);
+
+    ProductDto productElasticToProduct(ProductElastic productElastic);
+
+    List<ProductDto> mapToListProductDto(List<ProductElastic> productElastics);
+
 }
