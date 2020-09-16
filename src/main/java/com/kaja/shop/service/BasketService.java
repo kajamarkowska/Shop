d25913bb47d728004ec35eede45d3745649afc61
kajamarkowska <kajaa71@wp.pl>
2020-09-16 20:32:05 +0200

    added BasketService

--- src/main/java/com/kaja/shop/service/BasketService.java
+++ src/main/java/com/kaja/shop/service/BasketService.java
@@ -1,8 +1,13 @@
 package com.kaja.shop.service;
 
 import com.kaja.shop.domain.dto.ProductDto;
+import com.kaja.shop.domain.elastic.Basket;
 
 public interface BasketService {
 
     void addToBasket(ProductDto productDto);
+
+    Basket basketForUser();
+
+    void deleteProductById(Long id);
 }
