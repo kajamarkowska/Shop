c75919594c53c92cf4bd6761606c5a06a8e0931a
kajamarkowska <kajaa71@wp.pl>
2020-09-18 09:58:14 +0200

    added BasketController

---
+++ src/main/java/com/kaja/shop/controller/BasketController.java
@@ -0,0 +1,35 @@
+package com.kaja.shop.controller;
+
+import com.kaja.shop.domain.dto.ProductDto;
+import com.kaja.shop.mapper.ProductMapper;
+import com.kaja.shop.service.BasketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/basket")
+public class BasketController {
+
+    private final BasketService basketService;
+
+    private final ProductMapper productMapper;
+
+    @PostMapping
+    public void addProductToBasket(@RequestBody ProductDto productDto) {
+        basketService.addToBasket(productDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProductByIdInBasket(@PathVariable Long id) {
+        basketService.deleteProductById(id);
+    }
+
+    @GetMapping
+    public List<ProductDto> getUserBasket() {
+        return productMapper.mapToListProductDto(basketService.basketForUser().getProducts());
+    }
+
+}
