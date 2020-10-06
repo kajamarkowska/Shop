86266060e8be16c6e9e1b4d6679cfde6a57eab9c
kajamarkowska <kajaa71@wp.pl>
2020-10-06 09:02:25 +0200

    Added annotations

--- src/main/java/com/kaja/shop/ShopApplication.java
+++ src/main/java/com/kaja/shop/ShopApplication.java
@@ -1,5 +1,6 @@
 package com.kaja.shop;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,11 +10,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaAuditing
 @EnableSwagger2
 @EnableCaching
+@EnableBatchProcessing
 @SpringBootApplication
 public class ShopApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ShopApplication.class, args);
     }
+
+
 
 }
