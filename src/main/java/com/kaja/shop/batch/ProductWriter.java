a4ebaefa7bde291f08ea509195963610fee548bb
kajamarkowska <kajaa71@wp.pl>
2020-10-06 09:08:33 +0200

    ProductWriter has been modified

--- src/main/java/com/kaja/shop/batch/ProductWriter.java
+++ src/main/java/com/kaja/shop/batch/ProductWriter.java
@@ -7,11 +7,13 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class ProductWriter implements ItemWriter<Product> {
 
-    private ProductRepository productRepository;
+    private final ProductRepository productRepository;
+
     @Override
     public void write(List<? extends Product> list) throws Exception {
         productRepository.saveAll(list);
