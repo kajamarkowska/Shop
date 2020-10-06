5a76bddf00cfeaa518d3d3341a9d018720ff2915
kajamarkowska <kajaa71@wp.pl>
2020-10-06 09:10:46 +0200

    WatcherServiceImpl has been modified

--- src/main/java/com/kaja/shop/service/impl/WatcherServiceImpl.java
+++ src/main/java/com/kaja/shop/service/impl/WatcherServiceImpl.java
@@ -3,7 +3,12 @@ package com.kaja.shop.service.impl;
 import com.kaja.shop.config.BatchConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,7 +17,7 @@ import java.nio.file.*;
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class WatcherServiceImpl implements  Runnable{
+public class WatcherServiceImpl implements Runnable {
 
     private final JobLauncher jobLauncher;
     private final BatchConfig batchConfig;
@@ -25,11 +30,22 @@ public class WatcherServiceImpl implements  Runnable{
             path.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
             WatchKey watchKey;
             while ((watchKey = watchService.take()) != null) {
-
+                watchKey.pollEvents().forEach(event -> {
+                    Path filePath = Paths.get(path.toString(), event.context().toString());
+                    if (Files.isRegularFile(filePath)) {
+                        try {
+                            jobLauncher.run(batchConfig.csvFileToDatabaseJob(filePath.toString()), new JobParametersBuilder()
+                                    .addString("JobID", String.valueOf(System.currentTimeMillis())).toJobParameters());
+                        } catch (JobExecutionAlreadyRunningException | JobParametersInvalidException | JobInstanceAlreadyCompleteException | JobRestartException e) {
+                            log.error(e.getMessage(), e);
+                        }
+                    }
+                });
+                watchKey.reset();
             }
-            } catch(IOException | InterruptedException e){
-                log.error(e.getMessage(), e);
-            }
+        } catch (IOException | InterruptedException e) {
+            log.error(e.getMessage(), e);
+        }
 
     }
 }
