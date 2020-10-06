3632f3dff9b169419c356167e21ed58974d70666
kajamarkowska <kajaa71@wp.pl>
2020-10-06 09:14:06 +0200

    ProductElastic has been modified

--- src/main/java/com/kaja/shop/domain/elastic/ProductElastic.java
+++ src/main/java/com/kaja/shop/domain/elastic/ProductElastic.java
@@ -1,12 +1,14 @@
 package com.kaja.shop.domain.elastic;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder(toBuilder = true)
 public class ProductElastic {
     private Long id;
     private String name;
