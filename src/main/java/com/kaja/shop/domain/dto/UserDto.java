74bbc83f28a8898960576881ea0a682bfa39888e
kajamarkowska <kajaa71@wp.pl>
2020-11-05 10:53:59 +0100

    UserDto has been modified

--- src/main/java/com/kaja/shop/domain/dto/UserDto.java
+++ src/main/java/com/kaja/shop/domain/dto/UserDto.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.*;
 
@@ -16,8 +15,7 @@ import javax.validation.constraints.*;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
     private Long id;
-    @Email(message ="Invalid email" )
-
+    @Email(message = "Invalid email")
     private String email;
     @Size(min = 8, max = 255, message = "Invalid length password")
     @Pattern(regexp = "(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?\":{}|<>-]).{5,14}")
