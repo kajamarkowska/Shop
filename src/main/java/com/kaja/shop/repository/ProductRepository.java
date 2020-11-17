e29ec7978ca133b63fdb7198ba6d455a6193b8e5
kaja.markowska <kaja.markowska@wp.pl>
2020-11-17 15:25:37 +0100

    Query has been modified

--- src/main/java/com/kaja/shop/repository/ProductRepository.java
+++ src/main/java/com/kaja/shop/repository/ProductRepository.java
@@ -2,7 +2,12 @@ package com.kaja.shop.repository;
 
 import com.kaja.shop.domain.dao.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = "SELECT distinct name FROM product where name like '%?1%' order by name limit 5",nativeQuery = true)
+    List<String> autocomplete(String name);
 
 }
