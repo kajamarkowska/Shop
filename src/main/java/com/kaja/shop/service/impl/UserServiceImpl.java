fb02d2c4a568d7042e1afc0abd60b283a1746d4d
kaja.markowska <kaja.markowska@wp.pl>
2020-11-20 10:25:38 +0100

    the sending of e-mails after registration has been added

--- src/main/java/com/kaja/shop/service/impl/UserServiceImpl.java
+++ src/main/java/com/kaja/shop/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.kaja.shop.service.impl;
 import com.kaja.shop.domain.dao.User;
 import com.kaja.shop.repository.RoleRepository;
 import com.kaja.shop.repository.UserRepository;
+import com.kaja.shop.service.MailService;
 import com.kaja.shop.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Collections;
+import java.util.concurrent.Executors;
 
 @RequiredArgsConstructor
 @Service
@@ -24,11 +26,17 @@ public class UserServiceImpl implements UserService {
 
     private final RoleRepository roleRepository;
 
+    private final MailService mailService;
+
     @Override
     public User save(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         roleRepository.findByName("ROLE_USER").ifPresent(role -> user.setRoles(Collections.singleton(role)));
-        return userRepository.save(user);
+        try {
+            return userRepository.save(user);
+        } finally {
+            Executors.newSingleThreadExecutor().submit(() -> mailService.sendRegisterMail(user.getEmail()));
+        }
     }
 
     @Override
